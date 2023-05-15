f4614f41f3a142bbeeac7f872cd9c1341618053b
f3nrisw0lf <rodrickcorpuz@gmail.com>
2023-05-15 13:11:30 +0800

    Changed padding for Loading Component

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <section class='card shadow h-40 mt-20 text-center'>
+    <section class='card shadow py-14 px-5 mt-20 text-center'>
       <h1 class='text-4xl drop-shadow-md'>
         Please wait a moment while we prepare the best experience for you.
       </h1>
